a2c5842070a6f55ab43ccea7acccc45f80723d33
Ama <levonlevonyanxx@gmail.com>
2026-05-10 14:18:34 +0400

    fix(ci): declare firebase applet config module

---
+++ firebase-applet-config.json.d.ts
@@ -0,0 +1,12 @@
+declare const firebaseConfig: {
+  projectId: string;
+  appId: string;
+  apiKey: string;
+  authDomain: string;
+  firestoreDatabaseId?: string;
+  storageBucket?: string;
+  messagingSenderId?: string;
+  measurementId?: string;
+};
+
+export default firebaseConfig;
